fc4b29232f9eb42c32e7e504e4db810939c3112c
SBI- <pascal.huerlimann@gmail.com>
2017-03-23 09:55:17 +0100

    Initial commit with all development branches merged.

---
+++ src/main/resources/archetype-resources/plugin/src/main/java/IService.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import com.ibm.team.repository.common.transport.ITeamRestService;
+
+/**
+ *  Interface describing the service which the class loader loads.
+ *
+ */
+public interface IService extends ITeamRestService {
+}
