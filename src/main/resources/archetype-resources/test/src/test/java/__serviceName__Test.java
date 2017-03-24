1acff0ecb52764c83020a4e81726df5a7c4ff470
SBI- <pascal.huerlimann@gmail.com>
2017-03-24 15:29:24 +0100

    Add test module with surefire capabilities and an example test.

---
+++ src/main/resources/archetype-resources/test/src/test/java/__serviceName__Test.java
@@ -0,0 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ${serviceName}Test {
+
+    @Test
+    public void testTrue() throws Exception {
+        assertEquals(1, 1);
+    }
+}
+
