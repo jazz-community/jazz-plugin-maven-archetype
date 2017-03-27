5cb91b47dc834fecebb954521cf626b576a72697
SBI- <pascal.huerlimann@gmail.com>
2017-03-27 15:11:12 +0200

    Initial linux commit with same behaviour as ps version, but input sanitation.

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# read options, will exit if parameters are malformed
+temp=`getopt -n 'setup' -o g:s:v: --long group:,serviceName:,version: -- "$@"`
+
+eval set -- "$temp"
+
+# default parameters
+group="com.siemens.example"
+serviceName="ExampleService"
+version="1.0.0-SNAPSHOT"
+
+# handle parameters
+while true; do
+    case "$1" in
+        -g | --group        ) group="$2";       shift; shift;;
+        -s | --serviceName  ) serviceName="$2"; shift; shift;;
+        -v | --version      ) version="$2";     shift; shift;;
+        --                  )                   shift; break;;
+        *                   )                   break;;
+    esac
+done
+
+mvn clean install
+
+cd target
+
+mvn archetype:generate -B \
+    "-DarchetypeCatalog=local" \
+    "-DarchetypeGroupId=com.siemens.bt.jazz.services.archetype" \
+    "-DarchetypeArtifactId=com.siemens.bt.jazz.services.archetype.parent" \
+    "-Dversion=1.0.0-SNAPSHOT" \
+    "-DgroupId=$group" \
+    "-DartifactId=$group.parent" \
+    "-Dpackage=$group" \
+    "-DserviceName=$serviceName"
+
+cd "$group.parent"
+
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version "-DnewVersion=$version"
