b24fe00f52379908538319e2fda6cd64b7428c6d
SBI- <pascal.huerlimann@gmail.com>
2018-07-17 09:33:21 +0200

    Fix file format to google style

--- src/main/resources/archetype-resources/plugin/src/main/java/builder/HelloWorldPostService.java
+++ src/main/resources/archetype-resources/plugin/src/main/java/builder/HelloWorldPostService.java
@@ -1,34 +1,34 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.builder;
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+package ${package}.service;
 
 import com.ibm.team.repository.service.TeamRawService;
 import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
 import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
 import com.siemens.bt.jazz.services.base.utils.RequestReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.http.auth.AuthenticationException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 public final class HelloWorldPostService extends AbstractRestService {
 
-    public HelloWorldPostService(
-            Log log, HttpServletRequest request,
-            HttpServletResponse response,
-            RestRequest restRequest,
-            TeamRawService parentService,
-            PathParameters pathParameters) {
-        super(log, request, response, restRequest, parentService, pathParameters);
-    }
+  public HelloWorldPostService(
+      Log log, HttpServletRequest request,
+      HttpServletResponse response,
+      RestRequest restRequest,
+      TeamRawService parentService,
+      PathParameters pathParameters) {
+    super(log, request, response, restRequest, parentService, pathParameters);
+  }
 
-    public void execute() throws IOException, URISyntaxException, AuthenticationException {
-        String content = RequestReader.readAsString(request);
-        response.getWriter().write("POST successful! The following data was sent:\n" + content);
-    }
+  public void execute() throws IOException, URISyntaxException, AuthenticationException {
+    String content = RequestReader.readAsString(request);
+    response.getWriter().write("POST successful! The following data was sent:\n" + content);
+  }
 }
--- src/main/resources/archetype-resources/plugin/src/main/java/builder/HelloWorldService.java
+++ src/main/resources/archetype-resources/plugin/src/main/java/builder/HelloWorldService.java
@@ -1,38 +1,37 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.builder;
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+package ${package}.service;
 
+import com.ibm.team.repository.service.TeamRawService;
 import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
 import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URISyntaxException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.ibm.team.repository.service.TeamRawService;
 import org.apache.commons.logging.Log;
 import org.apache.http.auth.AuthenticationException;
 
+
 public class HelloWorldService extends AbstractRestService {
 
-    public HelloWorldService(
-            Log log, HttpServletRequest request,
-            HttpServletResponse response,
-            RestRequest restRequest,
-            TeamRawService parentService,
-            PathParameters pathParameters) {
-        super(log, request, response, restRequest, parentService, pathParameters);
-    }
+  public HelloWorldService(
+      Log log, HttpServletRequest request,
+      HttpServletResponse response,
+      RestRequest restRequest,
+      TeamRawService parentService,
+      PathParameters pathParameters) {
+    super(log, request, response, restRequest, parentService, pathParameters);
+  }
 
-    /**
-     * Execute the desired action and return a result
-     */
-    public void execute() throws IOException, URISyntaxException, AuthenticationException {
-        Writer writer = response.getWriter();
-        writer.write("Hello World!!!");
-    }
+  /**
+   * Execute the desired action and return a result
+   */
+  public void execute() throws IOException, URISyntaxException, AuthenticationException {
+    Writer writer = response.getWriter();
+    writer.write("Hello World!!!");
+  }
 }
